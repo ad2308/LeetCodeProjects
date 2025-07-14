a79e77c106295e4ae046177a2a16a4c4249432ca
Anuraag Datta <ad2308@cam.ac.uk>
2025-07-14 17:30:11 +0100

    part of p9

---
+++ src/P9.java
@@ -0,0 +1,17 @@
+class Solution9 {
+//    public boolean isPalindrome(int x) {
+//        String numString = String.valueOf(x);
+//        for (int i = 0; i < numString.length() / 2; i++) {
+//            if (numString.charAt(i) != numString.charAt(numString.length() - i - 1)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+
+    }
+}
